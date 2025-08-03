8bd787b9cce3aed1fc211e2369bc9d09fce938dc
ast <code@ast24.dev>
2025-08-03 10:02:09 +0900

    新構造整備

--- src/srv-www-ass/spalib/core/utils.ts
+++
@@ -1,7 +0,0 @@
-export async function delay(ms: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export async function yieldThread(): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, 0));
-}
