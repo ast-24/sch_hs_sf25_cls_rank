de8371cfedd93031ac951a6f73dd5ebec65a5167
ast <code@ast24.dev>
2025-08-19 09:29:10 +0900

    構文修正

--- src/srv-api/src/eps/dev-clear-all/get.mjs
+++ src/srv-api/src/eps/dev-clear-all/get.mjs
@@ -5,16 +5,14 @@ const SAFE_KEY = 'CM9h2NugsTxitUZtX9EPGpCTJvbRvi';
 export default async function (request, env) {
     if ((new URL(request.url)).searchParams.get('safe_key') === SAFE_KEY) {
         await (new TidbClient(env)).execInTx(async (cl) => {
-            await cl.query(`
-                DELETE FROM rankings_cache_round_latest;
-                DELETE FROM rankings_cache_round;
-                DELETE FROM rankings_cache_round_max;
-                DELETE FROM rankings_cache_total;
-                DELETE FROM rankings_cache_updated;
-                DELETE FROM users_rounds_answers;
-                DELETE FROM users_rounds;
-                DELETE FROM users;
-            `);
+            await cl.query('DELETE FROM rankings_cache_round_latest');
+            await cl.query('DELETE FROM rankings_cache_round');
+            await cl.query('DELETE FROM rankings_cache_round_max');
+            await cl.query('DELETE FROM rankings_cache_total');
+            await cl.query('DELETE FROM rankings_cache_updated');
+            await cl.query('DELETE FROM users_rounds_answers');
+            await cl.query('DELETE FROM users_rounds');
+            await cl.query('DELETE FROM users');
         });
         return new Response('OK', { status: 200 });
     } else {
